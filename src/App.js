91b3450133d0674183b3205f5fae788ab1900af5
MECATRONICA <mecatronica@iMac-de-MECATRONICA-84.local>
2023-09-22 18:58:17 -0600

    feat: Se agregaron la vista  de welcome y su css

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Welcome from './Screen/Welcome';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <Welcome/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Screen/Welcome/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './welcome.css';
+export default function index() {
+  return (
+    <div>
+        <h1>App con React</h1>
+        <p>Bienvenido a mi app donde podras revisar temas interesantes</p>
+        <button className='button-listado'>Ir al listado de tareas</button>
+        <button className='button-mas'>Mas sobre mí</button>
+    </div>
+  )
+}
